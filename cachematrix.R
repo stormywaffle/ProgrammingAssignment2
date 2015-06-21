2dac059c49233da164e63bade11474063d981193
stormywaffle <markcorre@gmail.com>
2015-06-21 12:08:27 -0700

    Finished cachematrix with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In order to solve for matrices using a cache you need to call the makeCacheMatrix function to store and create the matrix into the environment.
+## After that you can call cacheSolve to solve for the inverse of the matrix and if you already solved for cacheSolve will pull the inverse from
+## makeCacheMatrix rather than solving for it again.
 
-## Write a short comment describing this function
+## This function stores a matrix object. It also allows the ability to store its inverse saved into m. 
+## You can set the inverse by using setinverse and you can get it using set inverse. You can also 
+## set and get the matrix by using set and get respectively. 
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -18,9 +21,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function tries to get the inverse from the object by requesting the variable m from makeCacheMatrix using x's getinverse().
+## If there is nothing stored in m it means the cache is empty and m will be equal to null. If it is the if statement will not run
+## and solve will be called on the matrix and the result will be stored in m and m will be returned. If it is null then the message
+## "getting cached data" will print and the cached matrix will be returned.
 
-cacheSolve <- function(x = matrix()) {
+cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getinverse()
         if(!is.null(m)) {
